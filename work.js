02c6f566d4b1617a25c87b41ea357713cec2b277
Jerrick Snyder <runesword85@yahoo.com>
2023-01-19 10:09:01 -0500

    Update work.js

--- work.js
+++ work.js
@@ -7,17 +7,21 @@ setInterval(addCosts, 100);
 
 //vars
 const debug = false;
+const totalSpentElement = document.querySelector('#totalSpent');
+const difficulty = document.querySelector('#difficulty');
 const addNewRowButton = document.querySelector("#new");
 const testButton = document.querySelector("#test");
 const section = document.querySelector('section');
 const input = document.querySelector('#form');
-const costAt100percent = [180, 310, 530, 910, 1170, 2690, 4610];
+const costAt100percent = [180, 310, 530, 910, 1560, 2690, 4610, 7900];
 //cost for higher difficulty
 //=((100-difficulty) / 2) + 100
 const actualCosts = [];
 let numRows = 1;
 
 //main
+input.setAttribute('value', costAt100percent[0])
+difficulty.addEventListener('input', updateInputValue);
 addNewRowButton.addEventListener('click', addNewRow);
 testButton.addEventListener('click', test2);
 input.addEventListener('keyup', pressEnter)
@@ -27,13 +31,18 @@ function pressEnter(e){
     log(e)
     if (e.key == 'Enter'){
         addNewRow();
-        input.value = null;
     }
 }
 
 function addNewRow(){
     const newLine = document.createElement('p');
     let cost = document.querySelector('#form').value;
+    
+    if (cost == '' || cost == null || cost == NaN){
+        alert("Please enter a value greater than 1");
+        return;
+    }
+
     let buyTime = determineBuyTime(numRows, cost, 60)//TODO: fix hard coded max coin return value
     newLine.innerHTML = `${toOrdinal(numRows)}(cost: ${cost}) buy at <b>${buyTime}</b> coins`;
     section.appendChild(newLine);
@@ -42,6 +51,13 @@ function addNewRow(){
     numRows++;
     const inputSpan = document.querySelector('#order');
     inputSpan.innerText = toWordsOrdinal(numRows);
+
+    //store actual cost and update total
+    actualCosts.push(cost);
+    updateTotalSpent();
+
+    //populate new price guess
+    updateInputValue();
 }
 
 function addCosts(){
@@ -117,6 +133,31 @@ function coinsGenerated(currentCoins, maxCoinReturn, coinsForMaxBonus){
     let rawCoinsGenerated = maxCoinReturn * efficiency;
     let coinsGenerated = Math.floor(rawCoinsGenerated);
     return coinsGenerated
+}
+
+function updateInputValue(){
+
+    if (numRows > costAt100percent.length){
+        input.value = "";
+        return;
+    }
+    let currentPriceBase = costAt100percent[numRows - 1]
+    let currentMultiplier = (((difficulty.value - 100) / 2) + 100) / 100
+
+    if (currentMultiplier < 1){
+        currentMultiplier = 1;
+    }
+
+    let priceGuess = currentPriceBase * currentMultiplier;
+    input.setAttribute('value', priceGuess);
+}
+
+function updateTotalSpent(){
+    let totalSpent = 0;
+    for (let i of actualCosts){
+        totalSpent += parseInt(i);
+    }
+    totalSpentElement.innerText = totalSpent;
 }
 
 function test2(){
